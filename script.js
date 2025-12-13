e4a462df2de2f49a30fe2066397167b44f355a01
Ahmed Abdirizaak <mernstack.js@gmail.com>
2025-12-13 22:16:22 +0100

    Add: change-bg-color for test js

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 console.log("GitHub User Finder Script Loaded");
+
+const change_bg_color = document.getElementById("change-bg-color");
+
+function getRandomColor() {
+  return `rgb(
+    ${Math.floor(Math.random() * 256)},
+    ${Math.floor(Math.random() * 256)},
+    ${Math.floor(Math.random() * 256)}
+  )`;
+}
+
+change_bg_color.addEventListener("click", () => {
+  change_bg_color.style.backgroundColor = getRandomColor();
+});
